240506934fdd212b34f766055775eb4da50f30f8
purushottamtiwari934-ctrl <purushottamtiwari934@gmail.com>
2026-07-16 17:03:21 +0530

    Create     portfolio.ts

---
+++ data/portfolio.ts
@@ -0,0 +1,48 @@
+export const portfolio = [
+  {
+    id: 1,
+    title: "Future Sneaker Commercial",
+    type: "Concept Project",
+    category: "Commercial",
+
+    video: "/portfolio/future-sneaker.mp4",
+
+    thumbnail: "/portfolio/future-sneaker.jpg",
+
+    description:
+      "A futuristic AI-powered sneaker advertisement with cinematic storytelling."
+  },
+
+  {
+    id: 2,
+    title: "Luxury Skincare Launch",
+
+    type: "Studio Experiment",
+
+    category: "Product Visual",
+
+    video: "/portfolio/skincare.mp4",
+
+    thumbnail: "/portfolio/skincare.jpg",
+
+    description:
+      "Premium AI product launch film for a luxury skincare brand."
+  },
+
+  {
+    id: 3,
+
+    title: "Electric Vehicle Campaign",
+
+    type: "Spec Creative",
+
+    category: "Advertising",
+
+    video: "/portfolio/ev.mp4",
+
+    thumbnail: "/portfolio/ev.jpg",
+
+    description:
+      "Creative AI campaign designed for an electric mobility brand."
+  }
+];
